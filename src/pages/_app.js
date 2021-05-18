17f9ae416a334211f7eafb0e84435522d0f2a370
Rafael <rp.araujo2@gmail.com>
2021-05-18 10:50:02 -0300

    Remoção do amplify-cli

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,4 @@
 import '../styles/globals.css'
-import { Amplify } from "aws-amplify"
-import awsExports from "../aws-exports"
-Amplify.configure({ ...awsExports, ssr: true })
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,10 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import Amplify, { withSSRContext } from "aws-amplify"
-import config from "../../aws-exports.js";
-
-// Amplify SSR configuration needs to be done within each API route
-Amplify.configure({ ...config, ssr: true });
-
 export default (req, res) => {
   res.status(200).json({ name: 'John Doe' })
 }

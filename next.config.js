d5ae0ee89e9ff3be8a20ad457fec1400ad3a2197
Rafael <rp.araujo2@gmail.com>
2021-05-28 15:15:23 -0300

    deploy 34

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   env: {
-    REACT_APP_HOST: process.env.REACT_APP_HOST
+    REACT_APP_HOST: process.env.REACT_APP_HOST,
+    REACT_APP_SDK_API_KEY: process.env.REACT_APP_SDK_API_KEY,
+    REACT_APP_SDK_SECRET: process.env.REACT_APP_SDK_SECRET,
+    REACT_APP_SDK_BASE_URL: process.env.REACT_APP_SDK_BASE_URL,
   },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
